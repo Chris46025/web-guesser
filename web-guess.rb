1fdd183c1a21356ecc30b88c6f3d0de7b14d22e7
Antonio Bethune <abethune96@gmail.com>
2013-07-09 16:28:23 -0400

    basic guessing number game

--- web-guess.rb
+++ web-guess.rb
@@ -4,5 +4,18 @@ require 'sinatra/reloader'
 x= rand(101).to_s
 
 get "/" do 
-	erb :index, :locals => {:number=> x}
+	guess= params["guess"]
+	if guess.nil?
+		guess=""
+	elsif guess>x
+		guess= "Too high babydoll"
+		erb :index, :locals => {:number=> x, :message=> guess}
+	elsif guess<x
+		guess= "Too low babydoll"
+		erb :index, :locals => {:number=> x, :message=> guess}
+	else
+		guess= "Right on the money baby"
+		erb :index, :locals => {:number=> x, :message=> guess}	
+	end
+	erb :index, :locals => {:number=> x, :message=> guess}
 end
