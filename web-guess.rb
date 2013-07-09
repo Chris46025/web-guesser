631d8b6f70a32fb1d9cc75ca90e35a4a54ee4e43
Antonio Bethune <abethune96@gmail.com>
2013-07-09 14:35:30 -0400

    made random number

--- web-guess.rb
+++ web-guess.rb
@@ -1,10 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+x= rand(101).to_s
+
 get "/" do 
-	@date= Time.now.strftime('%a,  %b ,%d ,%Y')
-	@Tyhme= Time.now.strftime('%I:%M:%S %p')
-	"Hello World\n"+
-	"the date is "+@date+"\n"+
-	"the time is "+@Tyhme
+	
+	"The secret number is #{x}"
 end
