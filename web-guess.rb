eabfd75a4c391a09c2b558d36207b9af11142dc7
Antonio Bethune <abethune96@gmail.com>
2013-07-10 12:48:56 -0400

    gif with message

--- web-guess.rb
+++ web-guess.rb
@@ -1,21 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-x= rand(101).to_s
+x= rand(65).to_s
 
-get "/" do 
-	guess= params["guess"]
-	if guess.nil?
-		guess=""
-	elsif guess>x
-		guess= "Too high babydoll"
-		erb :index, :locals => {:number=> x, :message=> guess}
-	elsif guess<x
-		guess= "Too low babydoll"
-		erb :index, :locals => {:number=> x, :message=> guess}
-	else
-		guess= "Right on the money baby"
-		erb :index, :locals => {:number=> x, :message=> guess}	
-	end
-	erb :index, :locals => {:number=> x, :message=> guess}
+get "/" do
+guess= params["guess"]
+if guess.nil?
+guess=""
+elsif guess>x
+guess= "Too high babydoll"
+erb :index, :locals => {:number=> x, :message=> guess}
+elsif guess<x
+guess= "Too low babydoll"
+erb :index, :locals => {:number=> x, :message=> guess}
+else
+guess= "Right on the money baby"
+erb :index, :locals => {:number=> x, :message=> guess}	
+end
+erb :index, :locals => {:number=> x, :message=> guess}
 end
